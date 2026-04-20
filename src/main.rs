8ec7c860bf00eccc3e45635d9ce44ce840771440
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-20 07:44:53 +0300

    chore: rust quirk on crate names becoming root modules that can be implicitly defined and have ability to branch into submodules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
+    //in rust, a crate in kebab-case is imported in snake_case
+    //rust can use crate name as implicit root module, so we can directly call the function without importing it
+    //root module can chain to submodules, so we can directly call the function without importing it
     dev_purge::app::run()
 }
