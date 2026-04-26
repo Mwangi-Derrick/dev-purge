fd58c7db976c1e9e7b54976e30edcf4f6f0d18ac
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-26 20:24:08 +0300

    call the core module

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,5 +4,5 @@ fn main() -> Result<()> {
     //in rust, a crate in kebab-case is imported in snake_case
     //rust can use crate name as implicit root module, so we can directly call the function without importing it
     //root module can chain to submodules, so we can directly call the function without importing it
-    dev_purge::app::run()
+    dev_purge::core::run()
 }
